60322f5170d684962802d46fa348ec078697b9c1
LeColonDeJack <rosiere.mathieu@gmail.com>
2023-10-03 10:31:05 +0200

    mdpoublié trollface

---
+++ Js/connexion.js
@@ -0,0 +1,7 @@
+const audio = document.querySelector("#audio");
+audio.load();
+const picture = document.querySelector("#picture");
+
+picture.addEventListener("click", (e) => {
+  audio.play();
+});
